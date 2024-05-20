fba71d8428e27f4dde43f8d35891625ae5a4fe34
seoyeon1123 <lsy_0906@naver.com>
2024-05-21 00:05:20 +0900

    feat : hooks로 변경

--- 4.responseChecked/responseChecked hooks.jsx
+++ 4.responseChecked/responseChecked hooks.jsx
@@ -1,42 +1,42 @@
-import React, { useState } from 'react';
-import RenderAverage from './RenderAverage';
+import React, { useState, useRef } from 'react';
+import RenderAverage from './renderAverage';
 
 const ResponseChecked = () => {
   const [state, setState] = useState('waiting');
   const [message, setMessage] = useState('클릭해서 시작하세요');
   const [result, setResult] = useState([]);
-  const [timeoutId, setTimeoutId] = useState(null); // timeout 상태 추가
-  const [startTime, setStartTime] = useState(null);
-  const [endTime, setEndTime] = useState(null);
+  const timeOut = useRef(null);
+  const startTime = useRef();
+  const endTime = useRef();
 
   const onClickScreen = () => {
     if (state === 'waiting') {
       setState('ready');
       setMessage('초록색이 되면 눌러주세요');
-      const id = setTimeout(() => {
+      timeOut.current = setTimeout(() => {
         setState('now');
         setMessage('지금 클릭하세요!');
-        setStartTime(new Date());
+        startTime.current = new Date();
       }, Math.floor(Math.random() * 1000) + 2000);
-      setTimeoutId(id); // timeoutId 업데이트
     } else if (state === 'ready') {
-      // 성급하게 클릭
-      clearTimeout(timeoutId); // timeout 취소
+      clearTimeout(timeOut.current);
       setState('waiting');
       setMessage('너무 성급하시군요 ㅠㅠ 실패');
     } else if (state === 'now') {
-      setEndTime(new Date());
-      // 반응 속도 체크
+      endTime.current = new Date();
       setState('waiting');
       setMessage('클릭해서 시작하세요');
-      setResult([...result, endTime - startTime]);
+      setResult((prevResult) => [
+        ...prevResult,
+        endTime.current - startTime.current,
+      ]);
     }
   };
 
   const onReset = () => {
     setResult([]);
     setState('waiting');
-    setMessage('클릭해서 시작하세요 ');
+    setMessage('클릭해서 시작하세요');
   };
 
   return (
